0fe876976c389c212516b76a9bffdb4a4c5324dd
Muhammad Rabi <34184092+MuhammadRabi@users.noreply.github.com>
2020-12-30 13:42:21 +0200

    updating my project

--- weather-journal-app/README.md
+++ weather-journal-app/README.md
@@ -1,10 +1,28 @@
 # Weather-Journal App Project
 
 ## Overview
-This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
+
+This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI.
 
 ## Instructions
+
 This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
 
-## Extras
-If you are interested in testing your code as you go, you can use `tests.js` as a template for writing and running some basic tests for your code.
+###
+
+first of all I have selected my variables like apikey and url of api
+
+then I have added eventlistener on the button to trigger the process once it is clicked
+
+then I passed the required parameters in my callback function link myUrl and myApiKey
+
+and finally I created the updateUI function to write the retrieved data from server and right it down in html
+
+## server side
+
+1. I installed Node.js, Express and corr packages through NPM
+2. I aquired the packages to be operational.
+3. I have specified the current project file.
+4. I have established a server connection at port 8000 and listened to it.
+5. I constructed the GET and POST routes through the server and API
+6. I pushed all the data as once through my projectData array
--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,5 +1,81 @@
 /* Global Variables */
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let dateNew = new Date();
+let currentDate =
+  dateNew.getMonth() +
+  1 +
+  "." +
+  dateNew.getDate() +
+  "." +
+  dateNew.getFullYear();
+const myApiKey = "&appid=be83ab30c496c62c32ab82e92bef048b";
+const myBaseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+
+// adding the eventlistener when click generate trigger data retreival
+document.getElementById("generate").addEventListener("click", generateTemp);
+
+function generateTemp(e) {
+  const zipCode = document.getElementById("zip").value;
+  const myFeelings = document.getElementById("feelings").value;
+  if (zipCode == "") {
+    console.log("you must enter zip code");
+  }
+
+  retrieveInfo(myBaseURL, zipCode, myApiKey).then(function (data) {
+    console.log(data);
+    // add data to POST request
+    postData("/add", {
+      date: currentDate,
+      temp: data.main.temp,
+      content: myFeelings,
+    });
+    updateUi();
+  });
+}
+
+//function to GET web API data
+const retrieveInfo = async (myBaseURL, zipCode, myApiKey) => {
+  const res = await fetch(myBaseURL + zipCode + myApiKey);
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+// function to POST project data
+const postData = async (url = " ", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+// function to GET project data
+const updateUi = async () => {
+  const request = await fetch("/all");
+  try {
+    const allData = await request.json();
+    document.getElementById("date").innerHTML = `Date is ${allData[0].date}`;
+    document.getElementById(
+      "temp"
+    ).innerHTML = `Temperature is ${allData[0].temp}`;
+    document.getElementById(
+      "content"
+    ).innerHTML = `I feel ${allData[0].content}`;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
